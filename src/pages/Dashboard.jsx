1d7aa71e67ce7697b7fd52fa57eba098427b7435
coder_17 <tanupamsaha26@gmail.com>
2023-04-30 01:11:13 +0530

    Component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,25 @@
-import { useContext } from "react";
-import { EMSContext } from "../context/EMSContext";
-import { sayGreeting } from "../utils/helpers";
+// import { EMSContext } from "../context/EMSContext";
+import Sidebar from "../components/layout/Sidebar";
+import Profile from "../components/sections/Profile";
+import Main from "../components/sections/Main";
 
 const Dashboard = () => {
   const { state } = useContext(EMSContext);
   const { userData: user } = state;
-  const greeting = sayGreeting();
   return (
-    <div className="m-6">
-      Dashboard
-      <pre>{console.log(state)}</pre>
-      <h1 className="text-3xl font-semibold text-slate-900">
-        {greeting}, <span className="font-light text-brand">{user.name}</span>
-      </h1>
-      <img src={`data:image/png;base64,${user.profile_image}`} alt="" />
+    <div className="container mx-auto flex h-full flex-1 flex-row justify-evenly">
+      <div className="sidebar flex-col">
+        <Sidebar />
+      </div>
+      <div className="main flex flex-row">
+        <pre>{console.log(state)}</pre>
+        <Main />
+      </div>
+      <div className="profile">
+        <Profile />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
