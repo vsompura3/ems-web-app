9dc630527e458847530f13d782179763f5731c70
Vaibhav Sompura <vsompura3@gmail.com>
2023-05-04 11:15:59 +0530

    secular changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useReducer } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import { EMSContext, initialState } from "./context/EMSContext";
-import { reducer } from "./context/reducer";
-import About from "./pages/About";
-import Auth from "./pages/Auth";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Profile from "./pages/Profile";
-import Assignment from "./pages/Assignment";
-import Course from "./pages/Course";
-import Attendance from "./pages/Attendance";
-import Marks from "./pages/Marks";
-import Placement from "./pages/Placement";
+import { useReducer } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Header from './components/layout/Header'
+import { EMSContext, initialState } from './context/EMSContext'
+import { reducer } from './context/reducer'
+import About from './pages/About'
+import Assignment from './pages/Assignment'
+import Attendance from './pages/Attendance'
+import Auth from './pages/Auth'
+import Course from './pages/Course'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Marks from './pages/Marks'
+import NotFound from './pages/NotFound'
+import Placement from './pages/Placement'
+import Profile from './pages/Profile'
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <EMSContext.Provider value={{ state, dispatch }}>
       <BrowserRouter>
@@ -41,7 +41,7 @@ function App() {
         <Footer />
       </BrowserRouter>
     </EMSContext.Provider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 import {
+  getAnnouncements,
   getTimeTable,
+  getUser,
+  getUserAssignments,
   getUserAttendance,
   getUserMarks,
   getUserSyllabus,
@@ -12,6 +15,9 @@ import Profile from '../components/sections/Profile'
 import Timetable from '../components/sections/Timetable'
 import { EMSContext } from '../context/EMSContext'
 import {
+  GET_ANNOUNCEMENTS,
+  GET_PENDING_ASSIGNMENTS,
+  GET_PLACEMENT_DRIVES,
   GET_TIME_TABLE,
   GET_USER_ATTENDANCE,
   GET_USER_COURSE_SYLLABUS,
@@ -28,54 +34,101 @@ const Dashboard = () => {
   const { state, dispatch } = useContext(EMSContext)
   const [loader, setLoader] = useState(false)
 
+  useEffect(() => {
+    fetchTimeTable()
+    fetchAnnouncements()
+  }, [])
+
   const fetchTimeTable = async () => {
+    setLoader(true)
     const data = await getTimeTable(
       state.userCred.registrationNumber,
       state.userCred.password,
     )
     dispatch({ type: GET_TIME_TABLE, payload: data })
+    setLoader(false)
   }
-  useEffect(() => {
-    fetchTimeTable()
-  }, [])
 
   const fetchCourses = async () => {
+    setLoader(true)
     const data = await getUserSyllabus(
       state.userCred.registrationNumber,
       state.userCred.password,
     )
     dispatch({ type: GET_USER_COURSE_SYLLABUS, payload: data })
+    setLoader(false)
+  }
+
+  const fetchAnnouncements = async () => {
+    setLoader(true)
+    const data = await getAnnouncements(
+      state.userCred.registrationNumber,
+      state.userCred.password,
+    )
+    dispatch({ type: GET_ANNOUNCEMENTS, payload: data })
+    setLoader(false)
   }
 
   const fetchAttendance = async () => {
+    setLoader(true)
     const data = await getUserAttendance(
       state.userCred.registrationNumber,
       state.userCred.password,
     )
     dispatch({ type: GET_USER_ATTENDANCE, payload: data })
+    setLoader(false)
+  }
+
+  const fetchAssignments = async () => {
+    setLoader(true)
+    const data = await getUserAssignments(
+      state.userCred.registrationNumber,
+      state.userCred.password,
+    )
+    dispatch({ type: GET_PENDING_ASSIGNMENTS, payload: data })
+    setLoader(false)
+  }
+
+  const fetchPlacements = async () => {
+    setLoader(true)
+    const data = await getUser(
+      state.userCred.registrationNumber,
+      state.userCred.password,
+    )
+    dispatch({ type: GET_PLACEMENT_DRIVES, payload: data })
+    setLoader(false)
   }
 
   const fetchMarks = async () => {
+    setLoader(true)
     const data = await getUserMarks(
       state.userCred.registrationNumber,
       state.userCred.password,
     )
     dispatch({ type: GET_USER_MARKS, payload: data })
+    setLoader(false)
   }
 
   return (
     <div className="container mx-auto flex h-full flex-1 flex-row justify-stretch">
       <div className=" p-2">
-        <Sidebar />
+        <Sidebar
+          loader={loader}
+          fetchMarks={fetchMarks}
+          fetchAttendance={fetchAttendance}
+          fetchCourses={fetchCourses}
+          fetchAssignments={fetchAssignments}
+          fetchPlacements={fetchPlacements}
+        />
       </div>
       <div className="container mx-auto flex h-full flex-1 flex-row justify-between">
         {pathname === '/dashboard' && (
           <div className="flex flex-col">
             <div className="flex flex-row justify-between gap-4">
-              <div className="">
+              <div className="flex-[3]">
                 <Main />
               </div>
-              <div className="flex-wrap">
+              <div className="flex-wrap flex-[2]">
                 <Profile />
               </div>
             </div>
@@ -84,11 +137,11 @@ const Dashboard = () => {
             </div>
           </div>
         )}
-        {pathname === '/dashboard/assignment' && <Assignment />}
-        {pathname === '/dashboard/courses' && <Course />}
-        {pathname === '/dashboard/attendance' && <Attendance />}
-        {pathname === '/dashboard/marks' && <Marks />}
-        {pathname === '/dashboard/placement' && <Placement />}
+        {pathname === '/dashboard/assignment' && <Assignment loader={loader} />}
+        {pathname === '/dashboard/courses' && <Course loader={loader} />}
+        {pathname === '/dashboard/attendance' && <Attendance loader={loader} />}
+        {pathname === '/dashboard/marks' && <Marks loader={loader} />}
+        {pathname === '/dashboard/placement' && <Placement loader={loader} />}
       </div>
     </div>
   )
